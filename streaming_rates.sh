52e8022e3a0dfe0c53be16f220017c24fd47fef2
Omar Maghrabi <maghrabi.omar@gmail.com>
2019-01-12 04:36:11 -0500

    streams

---
+++ streaming_rates.sh
@@ -0,0 +1,1 @@
+curl   -H "Authorization: Bearer 1fee90dedad1abc90df403870923132c-7ec28839c346f9fc989abbf08c23d99b"   "https://stream-fxtrade.oanda.com/v3/accounts/001-001-1764820-001/pricing/stream?instruments=EUR_USD%2CGBP_USD" -s >curl-out
